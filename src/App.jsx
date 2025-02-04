6415ef6819d430db2db375e23e0963b11053d37a
Davide Chiarelli <davidechiarelli@MacBookPro.homenet.telecomitalia.it>
2025-02-04 14:15:57 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+//1) inserire l'import per bootstrap (da fare ancora)
+import { Container, Row , Col } from "react-bootstrap";
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <Container>
+      <Row>
+        <Col>
+        1
+        </Col>
+        <Col>
+        2
+        </Col>
+      </Row>
+    </Container>
+    </>
+  )
+}
+
+export default App
